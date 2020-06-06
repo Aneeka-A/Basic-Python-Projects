41cfd71e78e492715a6c7f14a64b10bd7397d68c
Aneeka-A <66246973+Aneeka-A@users.noreply.github.com>
2020-06-06 23:39:46 +0530

    Add files via upload

---
+++ addition_problems.py
@@ -0,0 +1,50 @@
+"""
+File: khansole_academy.py
+-------------------------
+Add your comments here.
+"""
+
+import random
+# Declaring the minimum and the maximum numbers that can appear in the question.
+MIN_NUM = 10
+MAX_NUM = 99
+# Declaring the number of times one has to give correct answers in a row to master the addition program.
+MASTER_ADD = 3
+
+def main():
+    """
+    This program will produce random addition problems
+    consisting of only two digit numbers that the user
+    will have to answer.
+    The program will have to keep producing problems until the
+    user masters the addition problems, i.e., until the user
+    answers 3 problems correctly in a row.
+    """
+    correct_in_a_row = 0
+    while correct_in_a_row < 3:
+
+        num_1 = random.randint(MIN_NUM, MAX_NUM)
+        num_2 = random.randint(MIN_NUM, MAX_NUM)
+        print("What is " + str(num_1) + " + " + str(num_2) + " ?")
+
+        expected_ans = int(num_1) + int(num_2)
+        expected_ans = int(expected_ans)
+
+        inp_ans = input("Your answer: ")
+        inp_ans = int(inp_ans)
+
+        if inp_ans == expected_ans:
+            correct_in_a_row += 1
+            print("Correct1 You've gotten {} correct in a row.".format(correct_in_a_row))
+
+        else:
+            correct_in_a_row = 0
+            print("Incorrect. The expected answer is " + str(expected_ans))
+
+    print("Congratulations! You mastered addition.")
+
+
+    # This provided line is required at the end of a Python file
+# to call the main() function.
+if __name__ == '__main__':
+    main()
